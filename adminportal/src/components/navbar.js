c7e3c9c4e38cea93b4501ee3f7176cb40e7c9afa
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-09-24 21:29:18 +0530

    update navbar

--- adminportal/src/components/navbar.js
+++ adminportal/src/components/navbar.js
@@ -1,5 +1,5 @@
-import { useHistory } from "react-router-dom"
-import React  from 'react';
+import { useHistory } from "react-router-dom";
+import React from "react";
 import {
   IconButton,
   Avatar,
@@ -10,7 +10,7 @@ import {
   VStack,
   Icon,
   useColorModeValue,
-  Link ,
+  Link,
   Drawer,
   DrawerContent,
   Text,
@@ -22,36 +22,35 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
-} from '@chakra-ui/react';
-import { HamburgerIcon,ChevronDownIcon,BellIcon,SmallAddIcon, AttachmentIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import {
+  HamburgerIcon,
+  ChevronDownIcon,
+  BellIcon,
+  SmallAddIcon,
+  AttachmentIcon,
+} from "@chakra-ui/icons";
 const LinkItems = [
-    { name: 'Add a admin', icon: SmallAddIcon, linkto:"/admin/addAdmin" },
-    { name: 'Provinces data', icon: AttachmentIcon,  linkto:"/admin/provinces"},
-  
-  ];
+  { name: "Add a admin", icon: SmallAddIcon, linkto: "/admin/addAdmin" },
+  { name: "Provinces data", icon: AttachmentIcon, linkto: "/admin/provinces" },
+];
 
-const logout1=()=>{
-    
-    console.log("sdsw")
-    fetch("/logout").
-    then(
-        // history.push("/admin/signin")
-    )
-}
-const NavBar=({
-  children,
-})=> {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const history=useHistory()
+const logout1 = () => {
+  console.log("sdsw");
+  fetch("/logout")
+    .then
+    // history.push("/admin/signin")
+    ();
+};
+const NavBar = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
 
-    
-    
- 
   return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
       <SidebarContent
         onClose={() => onClose}
-        display={{ base: 'none', md: 'block' }}
+        display={{ base: "none", md: "block" }}
       />
       <Drawer
         autoFocus={false}
@@ -60,7 +59,8 @@ const NavBar=({
         onClose={onClose}
         returnFocusOnClose={false}
         onOverlayClick={onClose}
-        size="full">
+        size="full"
+      >
         <DrawerContent>
           <SidebarContent onClose={onClose} />
         </DrawerContent>
@@ -72,24 +72,25 @@ const NavBar=({
       </Box>
     </Box>
   );
-}
+};
 
 const SidebarContent = ({ onClose, ...rest }) => {
   return (
     <Box
       transition="3s ease"
-      bg={useColorModeValue('white', 'gray.900')}
+      bg={useColorModeValue("white", "gray.900")}
       borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
+      borderRightColor={useColorModeValue("gray.200", "gray.700")}
+      w={{ base: "full", md: 60 }}
       pos="fixed"
       h="full"
-      {...rest}>
+      {...rest}
+    >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
         <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
           Logo
         </Text>
-        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
       {LinkItems.map((link) => (
         <NavItem key={link.name} icon={link.icon} link={link.linkto}>
@@ -100,10 +101,9 @@ const SidebarContent = ({ onClose, ...rest }) => {
   );
 };
 
-
 const NavItem = ({ icon, children, link, ...rest }) => {
   return (
-    <Link  href="#" style={{ textDecoration: 'none' }} href={link}>
+    <Link href="#" style={{ textDecoration: "none" }} href={link}>
       <Flex
         align="center"
         p="4"
@@ -112,16 +112,17 @@ const NavItem = ({ icon, children, link, ...rest }) => {
         role="group"
         cursor="pointer"
         _hover={{
-          bg: 'cyan.400',
-          color: 'white',
+          bg: "cyan.400",
+          color: "white",
         }}
-        {...rest}>
+        {...rest}
+      >
         {icon && (
           <Icon
             mr="4"
             fontSize="16"
             _groupHover={{
-              color: 'white',
+              color: "white",
             }}
             as={icon}
           />
@@ -132,7 +133,6 @@ const NavItem = ({ icon, children, link, ...rest }) => {
   );
 };
 
-
 const MobileNav = ({ onOpen, ...rest }) => {
   return (
     <Flex
@@ -140,13 +140,14 @@ const MobileNav = ({ onOpen, ...rest }) => {
       px={{ base: 4, md: 4 }}
       height="20"
       alignItems="center"
-      bg={useColorModeValue('white', 'gray.900')}
+      bg={useColorModeValue("white", "gray.900")}
       borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent={{ base: 'space-between', md: 'flex-end' }}
-      {...rest}>
+      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      justifyContent={{ base: "space-between", md: "flex-end" }}
+      {...rest}
+    >
       <IconButton
-        display={{ base: 'flex', md: 'none' }}
+        display={{ base: "flex", md: "none" }}
         onClick={onOpen}
         variant="outline"
         aria-label="open menu"
@@ -154,57 +155,60 @@ const MobileNav = ({ onOpen, ...rest }) => {
       />
 
       <Text
-        display={{ base: 'flex', md: 'none' }}
+        display={{ base: "flex", md: "none" }}
         fontSize="2xl"
         fontFamily="monospace"
-        fontWeight="bold">
+        fontWeight="bold"
+      >
         Logo
       </Text>
 
-      <HStack spacing={{ base: '0', md: '6' }}>
+      <HStack spacing={{ base: "0", md: "6" }}>
         <IconButton
           size="lg"
           variant="ghost"
           aria-label="open menu"
           icon={<BellIcon />}
         />
-        <Flex alignItems={'center'}>
+        <Flex alignItems={"center"}>
           <Menu>
             <MenuButton
               py={2}
               transition="all 0.3s"
-              _focus={{ boxShadow: 'none' }}>
+              _focus={{ boxShadow: "none" }}
+            >
               <HStack>
                 <Avatar
-                  size={'sm'}
+                  size={"sm"}
                   src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
                   }
                 />
                 <VStack
-                  display={{ base: 'none', md: 'flex' }}
+                  display={{ base: "none", md: "flex" }}
                   alignItems="flex-start"
                   spacing="1px"
-                  ml="2">
+                  ml="2"
+                >
                   <Text fontSize="sm">Justina Clark</Text>
                   <Text fontSize="xs" color="gray.600">
                     Admin
                   </Text>
                 </VStack>
-                <Box display={{ base: 'none', md: 'flex' }}>
+                <Box display={{ base: "none", md: "flex" }}>
                   <ChevronDownIcon />
                 </Box>
               </HStack>
             </MenuButton>
             <MenuList
-              bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}>
+              bg={useColorModeValue("white", "gray.900")}
+              borderColor={useColorModeValue("gray.200", "gray.700")}
+            >
               <MenuItem>Profile</MenuItem>
               <MenuItem>Settings</MenuItem>
               <MenuItem>Billing</MenuItem>
               <MenuDivider />
-              <MenuItem  onClick={()=>logout1()}>Sign out</MenuItem>
-             
+              <MenuItem onClick={() => logout1()}>Sign out</MenuItem>
             </MenuList>
           </Menu>
         </Flex>
