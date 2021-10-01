c6ae38ecf28d8872188ecc10acde259ecb513aee
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-10-02 01:56:01 +0530

    🙅‍♂️ No change

--- smart_travel_guide/App.js
+++ smart_travel_guide/App.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { AppProvider } from "./src/context/AppContext";
 import { Provider as PaperProvider } from "react-native-paper";
-import { StatusBar } from "react-native";
 import { theme } from "./src/core/theme";
 
 import { Navigation } from "./src/routes";
@@ -9,7 +8,6 @@ import { Navigation } from "./src/routes";
 function App() {
   return (
     <PaperProvider theme={theme}>
-      {/* <StatusBar animated={true} backgroundColor="#61dafb" /> */}
       <Navigation />
     </PaperProvider>
   );
