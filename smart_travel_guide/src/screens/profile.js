560a8c79269a72dda1653b4674c3f8b692d3ca38
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-10-02 02:01:43 +0530

    🛠 Add new styles to profile screen
    Detailed styling to profile screen and fix the navigation from logout and edit profile buttons.

--- smart_travel_guide/src/screens/profile.js
+++ smart_travel_guide/src/screens/profile.js
@@ -1,59 +1,79 @@
 import React, { useContext } from "react";
-import { Text, View } from "react-native";
-import { Avatar, Title, Headline, Button } from "react-native-paper";
+import { Text, View, StyleSheet } from "react-native";
+import { Avatar, Title, Headline, Button, Card } from "react-native-paper";
 import { AppContext } from "../context/AppContext";
-
+import Background from "../components/Background";
+import { theme } from "../core/theme";
 export function ProfileScreen(props) {
   const { onLogout, state } = useContext(AppContext);
-
+  const renderAvatar = () => {
+    if (state.pic) {
+      return <Avatar.Image size={150} source={{ uri: state.pic }} />;
+    } else {
+      return (
+        <Avatar.Icon
+          size={150}
+          icon="account"
+          color="white"
+          style={{ backgroundColor: theme.colors.primary }}
+        />
+      );
+    }
+  };
   return (
-    <View style={{ flex: 1, alignItems: "center", marginTop: 20 }}>
-      <Avatar.Image size={150} source={{ uri: state.pic }} />
-      <View style={{ flexDirection: "row", marginTop: 25 }}>
-        <Headline>First Name : </Headline>
-        <Title>{state.firstname}</Title>
-      </View>
-
-      <View style={{ flexDirection: "row" }}>
-        <Headline>Last Name : </Headline>
-        <Title>{state.lastname}</Title>
-      </View>
-
-      <View style={{ flexDirection: "row" }}>
-        <Headline>Email : </Headline>
-        <Title>{state.email}</Title>
-      </View>
-
-      <View style={{ flexDirection: "row" }}>
-        <Headline>DOB : </Headline>
-        <Title>{state.dob}</Title>
-      </View>
-
-      <View style={{ flexDirection: "row" }}>
-        <Headline>Country : </Headline>
-        <Title>{state.country}</Title>
-      </View>
-
-      <View style={{ flexDirection: "row" }}>
-        <Headline>Religion : </Headline>
-        <Title>{state.religion}</Title>
-      </View>
-
-      <View style={{ flexDirection: "row", marginBottom: 20 }}>
-        <Headline>Password : </Headline>
-        <Title>*******</Title>
-      </View>
-
-      <Button
-        icon="update"
-        mode="contained"
-        onPress={() => props.navigation.navigate("updateProfile")}
-      >
-        edit
-      </Button>
-      <Button icon="update" mode="contained" onPress={onLogout}>
-        Logout
-      </Button>
-    </View>
+    <Background>
+      {renderAvatar()}
+      <Card style={styles.card} elevation={8}>
+        <Card.Content style={{ alignItems: "center", marginVertical: 25 }}>
+          <Title>First Name : {state.firstname}</Title>
+          <Title>Last Name :{state.lastname}</Title>
+          <Title>Date of Birth : {state.dob}</Title>
+          <Title>Country : {state.country}</Title>
+          <Title>Religion : {state.religion}</Title>
+        </Card.Content>
+        <Card.Actions
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingHorizontal: 20,
+          }}
+        >
+          <Button
+            icon="update"
+            mode="contained"
+            labelStyle={{ color: "white" }}
+            style={{ borderRadius: 20, marginLeft: 10 }}
+            onPress={() => props.navigation.navigate("Edit Profile")}
+          >
+            edit
+          </Button>
+          <Button
+            icon="update"
+            mode="contained"
+            labelStyle={{ color: "white" }}
+            style={{
+              borderRadius: 20,
+              backgroundColor: "red",
+              marginRight: 10,
+            }}
+            onPress={() => {
+              onLogout();
+            }}
+          >
+            Logout
+          </Button>
+        </Card.Actions>
+      </Card>
+    </Background>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    width: "90%",
+    alignSelf: "center",
+    justifyContent: "space-between",
+    marginVertical: 12,
+    borderRadius: 20,
+  },
+});
