788ee21056bda3d4833fc79182a679640a547446
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-10-02 01:57:51 +0530

    🏗 Add a custom status bar
    A customizable status bar was added

--- smart_travel_guide/src/routes/AppNavigator.js
+++ smart_travel_guide/src/routes/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { StatusBar } from "expo-status-bar";
 import { CreatePlanNavigator } from "./CreatePlanNavigator";
 import { SavedPlanNavigator } from "./SavedPlanNavigator";
 import { FavouritesNavigator } from "./FavouritesNavigator";
@@ -12,45 +12,52 @@ const Tab = createBottomTabNavigator();
 
 export const AppNavigator = () => {
   return (
-    <Tab.Navigator
-      initialRouteName="Add Plan"
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: theme.colors.primary,
-        },
-        tabBarLabelStyle: { fontSize: 14, fontWeight: "bold" },
-        tabBarActiveTintColor: "black",
-        tabBarInactiveTintColor: "white",
-      }}
-    >
-      <Tab.Screen
-        name="Saved Plans"
-        component={SavedPlanNavigator}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="bookmark-outline" size={24} color={color} />
-          ),
+    <>
+      <Tab.Navigator
+        initialRouteName="Add Plan"
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: theme.colors.primary,
+          },
+          tabBarLabelStyle: { fontSize: 14, fontWeight: "bold" },
+          tabBarActiveTintColor: "black",
+          tabBarInactiveTintColor: "white",
         }}
-      />
-      <Tab.Screen
-        name="Add Plan"
-        component={CreatePlanNavigator}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="add-circle-outline" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Favourites"
-        component={FavouritesNavigator}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="favorite-outline" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      >
+        <Tab.Screen
+          name="Saved Plans"
+          component={SavedPlanNavigator}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="bookmark-outline" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Add Plan"
+          component={CreatePlanNavigator}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons
+                name="add-circle-outline"
+                size={24}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Popular"
+          component={FavouritesNavigator}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="star-outline" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+      <StatusBar style="inverted" backgroundColor="#8ed1c4" />
+    </>
   );
 };
