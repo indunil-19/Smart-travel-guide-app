c9ce9cd91aa9a3a2a9c816aede814476595f21e9
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-10-01 19:20:30 +0530

    🐛 Need to fix status bar
    Not yet implemented

--- smart_travel_guide/App.js
+++ smart_travel_guide/App.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { AppProvider } from "./src/context/AppContext";
 import { Provider as PaperProvider } from "react-native-paper";
-
+import { StatusBar } from "react-native";
 import { theme } from "./src/core/theme";
 
 import { Navigation } from "./src/routes";
@@ -9,6 +9,7 @@ import { Navigation } from "./src/routes";
 function App() {
   return (
     <PaperProvider theme={theme}>
+      {/* <StatusBar animated={true} backgroundColor="#61dafb" /> */}
       <Navigation />
     </PaperProvider>
   );
