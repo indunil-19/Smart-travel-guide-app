e9aa25ae883a937b283be2256b5cc7fb89806b71
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-10-14 17:42:23 +0530

    Update : New api key
    update viewpoiservice.js with new api key

--- smart_travel_guide/src/services/ViewPOIServices.js
+++ smart_travel_guide/src/services/ViewPOIServices.js
@@ -1,55 +1,58 @@
-const {Client} = require("@googlemaps/google-maps-services-js");
+const { Client } = require("@googlemaps/google-maps-services-js");
 
-export const getPoiData=async(place_id)=>{
-
-    const client = new Client({});
-    return await client.placeDetails({params:{
+export const getPoiData = async (place_id) => {
+  const client = new Client({});
+  return await client
+    .placeDetails({
+      params: {
         place_id: place_id,
-        key: "AIzaSyCB9FiwGVeEmdfBAwxiQpPuz0fsDMiwPWY",
-
-    }}).then((r)=>{
-       
-        return r.data.result
-    }).catch(e=>{
-        console.log(e)
+        key: "AIzaSyChMTwAb_hWwYdvcM_gSGcx84k_al-EtIA",
+      },
     })
-
-}
-
-export const getNearByPlaces=async(location)=>{
-    const client = new Client({});
-    return  client.placesNearby({
-        params:{
-            location:location,
-            key: "AIzaSyCB9FiwGVeEmdfBAwxiQpPuz0fsDMiwPWY",
-            type:['tourist_attraction', ],
-            radius:"30000",
-            
-        },
-        timeout: 1000
-    }).then((r)=>{
-        
-        return r.data.results
-    }).catch(e=>{
-        console.log(e)
+    .then((r) => {
+      return r.data.result;
     })
-}
+    .catch((e) => {
+      console.log(e);
+    });
+};
 
-export const getNearByHotels=async(location)=>{
-    const client = new Client({});
-    return  client.placesNearby({
-        params:{
-            location:location,
-            key: "AIzaSyCB9FiwGVeEmdfBAwxiQpPuz0fsDMiwPWY",
-            type:['lodging' ],
-            radius:"30000"
-        },
-        timeout: 1000
-    }).then((r)=>{
-        
-        return r.data.results
-    }).catch(e=>{
-        console.log(e)
+export const getNearByPlaces = async (location) => {
+  const client = new Client({});
+  return client
+    .placesNearby({
+      params: {
+        location: location,
+        key: "AIzaSyChMTwAb_hWwYdvcM_gSGcx84k_al-EtIA",
+        type: ["tourist_attraction"],
+        radius: "30000",
+      },
+      timeout: 1000,
     })
-}
+    .then((r) => {
+      return r.data.results;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
 
+export const getNearByHotels = async (location) => {
+  const client = new Client({});
+  return client
+    .placesNearby({
+      params: {
+        location: location,
+        key: "AIzaSyChMTwAb_hWwYdvcM_gSGcx84k_al-EtIA",
+        type: ["lodging"],
+        radius: "30000",
+      },
+      timeout: 1000,
+    })
+    .then((r) => {
+      return r.data.results;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
