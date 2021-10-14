88a80868b27dc8315c80f0e3d25d1d47d64480ac
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-10-14 17:48:53 +0530

    Update Utilites Update

--- smart_travel_guide/src/config/config.js
+++ smart_travel_guide/src/config/config.js
@@ -1,3 +1,3 @@
 export const Config = {
-  localhost: "http://3a4a-2402-4000-2180-4055-8542-4e04-a804-ebbb.ngrok.io",
+  localhost: "http://0523-2402-4000-2181-f9f9-4d6a-aa22-a280-a2b0.ngrok.io",
 };
