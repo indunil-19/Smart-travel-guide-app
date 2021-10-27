5910c5cfeee6643c32a5f5fd79d18ffc4db9b720
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-10-27 19:14:53 +0530

    Update : Utility
    - New packages
    - New Images

--- smart_travel_guide/src/config/config.js
+++ smart_travel_guide/src/config/config.js
@@ -1,3 +1,3 @@
 export const Config = {
-  localhost: "http://d526-2402-4000-2182-7e3d-2dc0-9132-6523-14c1.ngrok.io",
+  localhost: "http://fc0d-2402-4000-2181-f1e6-145b-c254-41ef-21d6.ngrok.io",
 };
