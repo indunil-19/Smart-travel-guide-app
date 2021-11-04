4b72a1dd66d39943a0aeee829d63b4d714e94108
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-11-05 02:12:32 +0530

    Add: Added Screens of FavourtitesNavigator
    Favourites.js
    TravelPlan.js
    LocationDetail.js
    .......

--- smart_travel_guide/src/routes/FavouritesNavigator.js
+++ smart_travel_guide/src/routes/FavouritesNavigator.js
@@ -1,26 +1,59 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TransitionPresets } from "@react-navigation/stack";
 
 import Favourites from "../screens/Favourites";
+import { TravelPlan } from "../screens/TravelPlan";
 import { ProfileScreen } from "../screens/profile";
 import { UpdateProfile } from "../screens/updateProfile";
+import { AddPlace } from "../screens/AddPlace";
+import { NearByHotels } from "../screens/NearByHotels";
+import { Route } from "../screens/Route";
+import LocationDetail from "../screens/LocationDetail.js";
 
 import { theme } from "../core/theme";
 import { HeaderAvatar } from "../components/HeaderAvatar";
+import { Notification } from "../components/Notification";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export const FavouritesNavigator = () => (
-  <Stack.Navigator
-    screenOptions={{
-      headerRight: () => <HeaderAvatar />,
-      headerStyle: {
-        backgroundColor: theme.colors.primary,
-      },
-    }}
-  >
-    <Stack.Screen name="Top Rated Plans" component={Favourites} />
-    <Stack.Screen name="Profile" component={ProfileScreen} />
-    <Stack.Screen name="Edit Profile" component={UpdateProfile} />
-  </Stack.Navigator>
+  <>
+    <Notification />
+    <Stack.Navigator
+      screenOptions={{
+        headerRight: () => <HeaderAvatar />,
+        headerRightContainerStyle: { padding: 10 },
+        headerStyle: {
+          backgroundColor: theme.colors.primary,
+        },
+      }}
+    >
+      <Stack.Screen name="Top Rated Plans" component={Favourites} />
+      <Stack.Screen
+        name="User Plan"
+        component={TravelPlan}
+        options={({ route }) => ({ title: route.params.title })}
+      />
+      <Stack.Screen name="Travel Route" component={Route} />
+
+      <Stack.Screen
+        name="Find Hotels"
+        component={NearByHotels}
+        options={{
+          presentation: "transparentModal",
+          headerShown: false,
+          cardOverlayEnabled: true,
+          ...TransitionPresets.ModalPresentationIOS,
+        }}
+      />
+      <Stack.Screen
+        name="Location Detail"
+        component={LocationDetail}
+        options={({ route }) => ({ title: route.params.name })}
+      />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Edit Profile" component={UpdateProfile} />
+    </Stack.Navigator>
+  </>
 );
--- smart_travel_guide/src/screens/Favourites.js
+++ smart_travel_guide/src/screens/Favourites.js
@@ -1,11 +1,102 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Text, View, Image, ScrollView, StyleSheet } from "react-native";
+import { ActivityIndicator, Snackbar } from "react-native-paper";
+import { Config } from "../config/config";
 import Background from "../components/Background";
+import { theme } from "../core/theme";
+import { PublicPlanCard } from "../components/PublicPlansCard";
 
-const Favourites = () => (
-  <Background>
-    <Text>Top Rated Plans</Text>
-  </Background>
-);
+const Favourites = () => {
+  const [plans, setPlans] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(`${Config.localhost}/user/getPublicTravelPlans`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setPlans(result.myPlans);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      {isLoading && (
+        <View style={styles.container}>
+          <ActivityIndicator animating={true} size={80} theme={theme} />
+        </View>
+      )}
+
+      {!isLoading && (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+          }}
+        >
+          <ScrollView>
+            {plans &&
+              plans.map((plan, index) => {
+                return (
+                  <View
+                    key={plan._id}
+                    style={{
+                      width: "100%",
+                      backgroundColor: "#fff",
+                      minHeight: 650,
+                    }}
+                  >
+                    <PublicPlanCard
+                      _id={plan._id}
+                      name={plan.name ? plan.name : "Travel Sri Lanka"}
+                      days={plan.travelPlan[0].length}
+                      createdDate={plan.createdAt}
+                      travelPlan={plan.travelPlan}
+                    />
+                  </View>
+                );
+              })}
+          </ScrollView>
+
+          {plans && plans.length == 0 ? (
+            <View
+              style={{
+                height: "100%",
+                backgroundColor: "#fff",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingHorizontal: 10,
+                paddingTop: 10,
+              }}
+            >
+              <Image
+                style={{ width: 250, height: 250 }}
+                source={require("../assets/img/travel.png")}
+              />
+              <Text
+                style={{
+                  fontSize: 20,
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+              >
+                Seems there aren't any shared plans
+              </Text>
+            </View>
+          ) : (
+            <></>
+          )}
+        </View>
+      )}
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+  },
+});
 
 export default Favourites;
