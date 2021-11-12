14e3cb42162a4e2aca4f02712314ca00702040bf
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2021-11-13 00:46:35 +0530

    UPDATE : Utility

--- smart_travel_guide/src/config/config.js
+++ smart_travel_guide/src/config/config.js
@@ -1,4 +1,4 @@
 export const Config = {
-  localhost: "http://2991-2402-4000-2182-1735-3d0e-3be-4e7a-b8f5.ngrok.io",
+  localhost: "https://smart-guide-app.herokuapp.com",
   apiKey: "AIzaSyCZvOdE2r9moeOBqDlvgP1PH3n-v6ZUWVQ",
 };
